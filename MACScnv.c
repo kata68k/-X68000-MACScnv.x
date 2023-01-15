ee84062f355fb338bfbb4b8aca0d49287ef563bf
kata68k <kata68k@gmail.com>
2023-01-15 21:53:23 +0900

    改造版MACS機能削除とTpリンクがないバグ修正

--- MACScnv.c
+++ MACScnv.c
@@ -2,20 +2,17 @@
 #include <stdlib.h>
 #include <string.h>
 #include <math.h>
-#include "../../../inc/usr_style.h"
-#include "../../../inc/usr_macro.h"
-#include "../../../inc/usr_define.h"
+#include "usr_style.h"
+#include "usr_macro.h"
+#include "usr_define.h"
 
 #define VSYNC_sec	(0.01803177)/*(1.0f/55.45767f)*/
 
 void HelpMessage(void);
-void HelpMessage2(void);
 int16_t CGcnv(int16_t, int16_t, int16_t);
 int16_t BLTcnv(int16_t, int16_t, int16_t, int16_t);
 int16_t MACScnv(int16_t, int16_t, int16_t, int16_t, int16_t);
 int16_t main(int16_t, int8_t**);
-int32_t g_nEffect = 0;
-int32_t g_nEffectType = 0;
 
 void HelpMessage(void)
 {
@@ -39,37 +36,6 @@ void HelpMessage(void)
 	printf("------------------------------\n");
 }
 
-void HelpMessage2(void)
-{
-	printf("MACScnv.x -S[n] <開始番号> <増加値> <終了番号> <動画再生時間[s] or PCMの時間[s]>\n");
-	printf("ex. 0～1620まで存在する画像ファイルを4つずつ飛ばして、長さ81秒のMACSデータを作る場合\n");
-	printf(">MACScnv.x -S1 0 4 1620 81\n");
-	printf("------------------------------\n");
-	printf("n=0 256x256 65536色  \n");
-	printf("n=1 256x256 256色  \n");
-	printf("n=2 384x256 256色  \n");
-	printf("n=3 512x512 16色  \n");
-	printf("n=4 512x512 4色  \n");
-	printf("n=5 768x512 1色  \n");
-	printf("------------------------------\n");
-	printf("変換に必要なツール＆ファイル：\n");
-	printf("=< 0_MakeTxTp.bat >=============\n");
-	printf("    画像表示　　　　：APICG.r\n");
-	printf("    画像保存　　　　：gvrsave.x\n");
-	printf("=< 1_MakePCM.bat >==============\n");
-	printf("    Wav to ADPCM　　：pcm3pcm.x\n");
-	printf("=< 2_MakeLZE.bat >==============\n");
-	printf("    画像圧縮　　　　：lze.x / lze.exe\n");
-	printf("=< 3_MakeBLK.bat >==============\n");
-	printf("    データオブジェクト生成　　：has060.x\n");
-	printf("=< 4_MakeMACS.bat >=============\n");
-	printf("    MACSヘッダ　　　：MACS_SCH.h\n");
-	printf("    アセンブラ　　　：has060.x\n");
-	printf("    リンカ　　　　　：hlk.r\n");
-	printf("    MACSデータ変換　：MakeMCS.x\n");
-	printf("------------------------------\n");
-}
-
 int16_t CGcnv(int16_t start_cnt, int16_t inc_val, int16_t end_cnt)
 {
 	FILE *fp;
@@ -84,28 +50,7 @@ int16_t CGcnv(int16_t start_cnt, int16_t inc_val, int16_t end_cnt)
 							};
 
 	/* ファイルを開ける */
-	if(g_nEffect == TRUE)
-	{
-		switch(g_nEffectType)
-		{
-		case 0:
-		case 5:
-			strcpy(fname, "0_MakeTx.bat");
-			break;
-		case 1:
-		case 2:
-		case 3:
-		case 4:
-			strcpy(fname, "0_MakeTxTp.bat");
-			break;
-		default:
-			break;
-		}
-	}
-	else
-	{
-		strcpy(fname, "0_MakeTxTp.bat");
-	}
+	strcpy(fname, "0_MakeTxTp.bat");
 	
 	fp = fopen(fname, "r");
 	if(fp != NULL)
@@ -119,45 +64,15 @@ int16_t CGcnv(int16_t start_cnt, int16_t inc_val, int16_t end_cnt)
 	
 	fp = fopen(fname, "w");
 	/* ファイルに書き込む */
-	if(g_nEffect == TRUE)
-	{
-		fprintf(fp, "\n");
-	}
-	else
-	{
-		fprintf(fp, "%s\n\n", sStingDef);
-	}
+	fprintf(fp, "%s\n\n", sStingDef);
 	
 	for(cnt = start_cnt; cnt <= end_cnt; cnt += inc_val)
 	{
 		/* ファイルに書き込む */
 		fprintf(fp, "%s CG%05d.bmp\n", sStingMes[0], cnt);
 		
-		if(g_nEffect == TRUE)
-		{
-			fprintf(fp, "\n");
-			
-			switch(g_nEffectType)
-			{
-			case 0:
-			case 5:
-				fprintf(fp, "gvrsave -s%d -oTx%05d\n\n", g_nEffectType, cnt, cnt);
-				break;
-			case 1:
-			case 2:
-			case 3:
-			case 4:
-				fprintf(fp, "gvrsave -s%d -oTx%05d -pTp%05d\n\n", g_nEffectType, cnt, cnt);
-				break;
-			default:
-				break;
-			}
-		}
-		else
-		{
-			fprintf(fp, "%s\n", sStingMes[1]);
-			fprintf(fp, "%s Tx%05d Tp%05d\n\n", sStingMes[2], cnt, cnt);
-		}
+		fprintf(fp, "%s\n", sStingMes[1]);
+		fprintf(fp, "%s Tx%05d Tp%05d\n\n", sStingMes[2], cnt, cnt);
 	}
 
 	/* ファイルを閉じる */
@@ -182,41 +97,12 @@ int16_t CGcnv(int16_t start_cnt, int16_t inc_val, int16_t end_cnt)
 	fp = fopen(fname, "w");
 	/* ファイルに書き込む */
 	fprintf(fp, "pcm3pcm.x AD.WAV AD.PCM\n", sStingDef);
-	if(g_nEffect == TRUE)
-	{
-		fprintf(fp, "pcm3pcm.x AD.WAV AD.s44\n", sStingDef);
-	}
 	/* ファイルを閉じる */
 	fclose (fp);
 	
 	printf("%s を作成しました。\n",fname);
 	printf("---------------------------\n");
 	
-	if(g_nEffect == TRUE)
-	{
-		/* ファイルを開ける */
-		strcpy(fname, "2_MakeLZE.bat");
-		fp = fopen(fname, "r");
-		if(fp != NULL)
-		{
-			printf("ファイルが既に存在します。(%s)\n", fname);
-			printf("__Clean.bat を実行してください\n");
-			/* ファイルを閉じる */
-			fclose (fp);
-			return -1;
-		}
-		
-		fp = fopen(fname, "w");
-		/* ファイルに書き込む */
-		fprintf(fp, "for %%%%i in (Tx*.) do lze e %%%%i %%%%i.lze\n", sStingDef);
-		
-		/* ファイルを閉じる */
-		fclose (fp);
-		
-		printf("%s を作成しました。\n",fname);
-		printf("---------------------------\n");
-	}
-
 	return ret;
 }
 
@@ -233,39 +119,12 @@ int16_t BLTcnv(int16_t start_cnt, int16_t inc_val, int16_t end_cnt, int16_t mode
 	flag = 0;
 	cnt = start_cnt;
 	
-	if(mode == 1)
-	{
-		if(g_nEffect == TRUE)
-		{
-			switch(g_nEffectType)
-			{
-			case 0:
-			case 5:
-				return ret;
-			case 1:
-			case 2:
-			case 3:
-			case 4:
-				break;
-			default:
-				break;
-			}
-		}
-	}
-	
 	switch(mode)
 	{
 		case 2:
 		{
 			/* ファイルを開ける */
-			if(g_nEffect == TRUE)
-			{
-				strcpy(fname, "LIST_PCM.s");
-			}
-			else
-			{
-				strcpy(fname, "LIST_PCM.blk");
-			}
+			strcpy(fname, "LIST_PCM.blk");
 			fp = fopen(fname, "r");
 			if(fp != NULL)
 			{
@@ -279,18 +138,8 @@ int16_t BLTcnv(int16_t start_cnt, int16_t inc_val, int16_t end_cnt, int16_t mode
 			fp = fopen(fname, "w");
 			
 			/* ファイルに書き込む */
-			if(g_nEffect == TRUE)
-			{
-				fprintf(fp, "pcmdat:: .insert AD.S44\n");
-				fprintf(fp, "pcmend::\n");
-				fprintf(fp, "adpcmdat:: .insert AD.PCM\n");
-				fprintf(fp, "adpcmend::\n");
-			}
-			else
-			{
-				fprintf(fp, "pcmdat:AD.PCM\n");
-				fprintf(fp, "pcmend:\n");
-			}
+			fprintf(fp, "pcmdat:AD.PCM\n");
+			fprintf(fp, "pcmend:\n");
 
 			/* ファイルを閉じる */
 			fclose (fp);
@@ -303,14 +152,8 @@ int16_t BLTcnv(int16_t start_cnt, int16_t inc_val, int16_t end_cnt, int16_t mode
 		case 3:
 		{
 			/* ファイルを開ける */
-			if(g_nEffect == TRUE)
-			{
-				strcpy(fname, "3_MakeBLK.bat");
-			}
-			else
-			{
-				strcpy(fname, "2_MakeBLK.bat");
-			}
+			strcpy(fname, "2_MakeBLK.bat");
+
 			fp = fopen(fname, "r");
 			if(fp != NULL)
 			{
@@ -324,38 +167,12 @@ int16_t BLTcnv(int16_t start_cnt, int16_t inc_val, int16_t end_cnt, int16_t mode
 			fp = fopen(fname, "w");
 			
 			/* ファイルに書き込む */
-			if(g_nEffect == TRUE)
+			for(cnt = start_cnt; cnt <= end_cnt; cnt += inc_val)
 			{
-				for(cnt = start_cnt; cnt <= end_cnt; cnt += inc_val)
-				{
-					fprintf(fp, "has060 LIST%d_Tx.s\n", cnt);
-					
-					switch(g_nEffectType)
-					{
-					case 0:
-					case 5:
-						break;
-					case 1:
-					case 2:
-					case 3:
-					case 4:
-						fprintf(fp, "has060 LIST%d_Tp.s\n", cnt);
-						break;
-					default:
-						break;
-					}
-				}
-				fprintf(fp, "has060 LIST_PCM.s\n");
+				fprintf(fp, "blk LIST%d_Tx.blk\n", cnt);
+				fprintf(fp, "blk LIST%d_Tp.blk\n", cnt);
 			}
-			else
-			{
-				for(cnt = start_cnt; cnt <= end_cnt; cnt += inc_val)
-				{
-					fprintf(fp, "blk LIST%d_Tx.blk\n", cnt);
-					fprintf(fp, "blk LIST%d_Tp.blk\n", cnt);
-				}
-				fprintf(fp, "blk LIST_PCM.blk\n");
-			}
+			fprintf(fp, "blk LIST_PCM.blk\n");
 
 			/* ファイルを閉じる */
 			fclose (fp);
@@ -377,27 +194,13 @@ int16_t BLTcnv(int16_t start_cnt, int16_t inc_val, int16_t end_cnt, int16_t mode
 			default:
 			case 0:
 			{
-				if(g_nEffect == TRUE)
-				{
-					sprintf(fname, "LIST%d_Tx.s", i);
-				}
-				else
-				{
-					sprintf(fname, "LIST%d_Tx.blk", i);
-				}
+				sprintf(fname, "LIST%d_Tx.blk", i);
 				strcpy(str, "Tx");
 				break;
 			}
 			case 1:
 			{
-				if(g_nEffect == TRUE)
-				{
-					sprintf(fname, "LIST%d_Tp.s", i);
-				}
-				else
-				{
-					sprintf(fname, "LIST%d_Tp.blk", i);
-				}
+				sprintf(fname, "LIST%d_Tp.blk", i);
 				strcpy(str, "Tp");
 				break;
 			}
@@ -415,23 +218,8 @@ int16_t BLTcnv(int16_t start_cnt, int16_t inc_val, int16_t end_cnt, int16_t mode
 		fp = fopen(fname, "w");
 		do
 		{
-			if(g_nEffect == TRUE)
-			{
-				/* ファイルに書き込む */
-				if(mode == 0)
-				{
-					fprintf(fp, "%s%05d:: .insert %s%05d.lze\n", str, cnt, str, cnt);
-				}
-				else
-				{
-					fprintf(fp, "%s%05d:: .insert %s%05d\n", str, cnt, str, cnt);
-				}
-			}
-			else
-			{
-				/* ファイルに書き込む */
-				fprintf(fp, "%s%05d:%s%05d\n", str, cnt, str, cnt);
-			}
+			/* ファイルに書き込む */
+			fprintf(fp, "%s%05d:%s%05d\n", str, cnt, str, cnt);
 			
 			cnt += inc_val;
 			
@@ -475,13 +263,6 @@ int16_t MACScnv(int16_t start_cnt, int16_t inc_val, int16_t end_cnt, int16_t mod
 	float f_frame, f_fps, f_spf, f_time, f_r_diff, f_r_diff_old;
 	float r_time, r_time_old;
 	float f1, f2;
-	int8_t sStingCMD[6][10] = {	"_G64K",
-								"_G256",
-								"_G384",
-								"_T512_C16",
-								"_T512_C4",
-								"_T768",
-							};
 
 	f_time = (float)0;
 	f_r_diff = (float)0;
@@ -513,17 +294,6 @@ int16_t MACScnv(int16_t start_cnt, int16_t inc_val, int16_t end_cnt, int16_t mod
 			fprintf(fp, ".include MACS_SCH.h\n\n");
 
 			fprintf(fp, "SET_OFFSET\n\n");
-			
-			if(g_nEffect == TRUE)
-			{
-				fprintf(fp, "USE_DUALPCM 'S44'\n");
-				fprintf(fp, "TITLE ''\n");
-				fprintf(fp, "COMMENT ''\n\n");
-
-				fprintf(fp, "SCREEN_ON%s\n\n", sStingCMD[g_nEffectType]);
-				
-				fprintf(fp, "CHANGE_POSITION%s\n\n", sStingCMD[g_nEffectType]);
-			}
 			
 			f_frame = (float)frame / inc_val;		/* 演算対象フレーム総数の算出[f] */
 			f_fps = f_frame / (float)sec;			/* 一秒あたり何フレームなのか？算出[fps] */
@@ -571,62 +341,19 @@ int16_t MACScnv(int16_t start_cnt, int16_t inc_val, int16_t end_cnt, int16_t mod
 				
 				if( (r_count > 0) || (cnt == start_cnt) )
 				{
-					if(g_nEffect == TRUE)
+					fprintf(fp, "DRAW Tx%05d\n", cnt);
+					if(cnt != start_cnt)
 					{
-						fprintf(fp, "DRAW%s Tx%05d\n", sStingCMD[g_nEffectType], cnt);
-						if(cnt != start_cnt)
-						{
-							fprintf(fp, "WAIT %d\n", (int16_t)r_count);
-						}
-						else
-						{
-							fprintf(fp, "WAIT %d\n", (int16_t)b_frame);
-						}
-						
-						switch(g_nEffectType)
-						{
-						case 0:
-						case 5:
-							/* none */
-							break;
-						case 1:
-						case 2:
-							fprintf(fp, "PALETTE256 Tp%05d\n", cnt);
-							break;
-						case 3:
-						case 4:
-							fprintf(fp, "PALETTE16 Tp%05d\n", cnt);
-							break;
-						default:
-							break;
-						}
-						
-						fprintf(fp, "CHANGE_POSITION%s\n\n", sStingCMD[g_nEffectType]);
+						fprintf(fp, "WAIT %d\n", (int16_t)r_count);
 					}
-					else
-					{
-						fprintf(fp, "DRAW Tx%05d\n", cnt);
-						if(cnt != start_cnt)
-						{
-							fprintf(fp, "WAIT %d\n", (int16_t)r_count);
-						}
-						fprintf(fp, "PALETTE Tp%05d\n", cnt);
-						fprintf(fp, "CHANGE_POSITION\n\n");
-					}
+					fprintf(fp, "PALETTE Tp%05d\n", cnt);
+					fprintf(fp, "CHANGE_POSITION\n\n");
 				}
 				
 				if(cnt == start_cnt)
 				{
-					if(g_nEffect == TRUE)
-					{
-						fprintf(fp, "PCM_PLAY_S44 pcmdat,pcmend-pcmdat\n");
-						fprintf(fp, "PCM_PLAY_SUBADPCM adpcmdat,adpcmend-adpcmdat\n\n");
-					}
-					else
-					{
-						fprintf(fp, "SCREEN_ON\n\n");
-						fprintf(fp, "PCM_PLAY pcmdat,pcmend-pcmdat\n\n");
-					}
+					fprintf(fp, "SCREEN_ON\n\n");
+					fprintf(fp, "PCM_PLAY pcmdat,pcmend-pcmdat\n\n");
 				}
 				
 			}
@@ -644,23 +371,16 @@ int16_t MACScnv(int16_t start_cnt, int16_t inc_val, int16_t end_cnt, int16_t mod
 		}
 		case 1:
 		{
-			int8_t sStingMes[6][32] = {	"has -u MACSsrc",
+			int8_t sStingMes[6][32] = {	"has -t . -u MACSsrc",
 										"hlk -r MACSsrc",
 										"MakeMCS MACSsrc",
-										"has060 -u MACSsrc",
+										"has060 -t . -u MACSsrc",
 										"060high 1",
 										"060high 0"
 									};
 			
 			/* ファイルを開ける */
-			if(g_nEffect == TRUE)
-			{
-				strcpy(fname, "4_MakeMACS.bat");
-			}
-			else
-			{
-				strcpy(fname, "3_MakeMACS.bat");
-			}
+			strcpy(fname, "3_MakeMACS.bat");
 			fp = fopen(fname, "r");
 			if(fp != NULL)
 			{
@@ -674,41 +394,18 @@ int16_t MACScnv(int16_t start_cnt, int16_t inc_val, int16_t end_cnt, int16_t mod
 			fp = fopen(fname, "w");
 			
 			/* ファイルに書き込む */
-			if(g_nEffect == TRUE)
-			{
-				fprintf(fp, "%s\n", sStingMes[3]);
-				fprintf(fp, "%s\n", sStingMes[4]);
-			}
-			else
-			{
-				fprintf(fp, "%s\n", sStingMes[0]);
-			}
+			fprintf(fp, "%s\n", sStingMes[0]);
+			
+			fprintf(fp, "%s\n", sStingMes[4]);
 			fprintf(fp, "%s ", sStingMes[1]);
 			for(cnt = start_cnt; cnt <= end_cnt; cnt += inc_val)
 			{
 				fprintf(fp, "LIST%d_Tx ", cnt);
-				switch(g_nEffectType)
-				{
-				case 0:
-				case 5:
-					/* none */
-					break;
-				case 1:
-				case 2:
-				case 3:
-				case 4:
-					fprintf(fp, "LIST%d_Tp ", cnt);
-					break;
-				default:
-					break;
-				}
+				fprintf(fp, "LIST%d_Tp ", cnt);
 			}
 			fprintf(fp, "LIST_PCM\n\n");
+			fprintf(fp, "%s\n", sStingMes[5]);
 			fprintf(fp, "%s\n", sStingMes[2]);
-			if(g_nEffect == TRUE)
-			{
-				fprintf(fp, "%s\n", sStingMes[5]);
-			}
 
 			/* ファイルを閉じる */
 			fclose (fp);
@@ -729,6 +426,8 @@ int16_t main(int16_t argc, int8_t** argv)
 	int16_t ret = 0;
 	int16_t cnt[4];
 	
+	puts("MACSデータ作成補助ツール「MACScnv.x」v1.0.4 (c)2022-2023 カタ.");
+	
 	if(argc > 1)	/* オプションチェック */
 	{
 		int16_t i;
@@ -736,7 +435,7 @@ int16_t main(int16_t argc, int8_t** argv)
 		for(i = 1; i < argc; i++)
 		{
 			int8_t	bOption;
-			int8_t	bFlag, bFlag2;
+			int8_t	bFlag;
 			
 			bOption	= ((argv[i][0] == '-') || (argv[i][0] == '/')) ? TRUE : FALSE;
 
@@ -747,47 +446,7 @@ int16_t main(int16_t argc, int8_t** argv)
 				if(bFlag == TRUE)
 				{
 					HelpMessage();	/* ヘルプ */
-					HelpMessage2();	/* ヘルプ2 */
 					ret = -1;
-				}
-				
-				bFlag	= ((argv[i][1] == 's') || (argv[i][1] == 'S')) ? TRUE : FALSE;
-				bFlag2	= strlen(&argv[i][2]);
-				if((bFlag == TRUE) && (bFlag2 > 0))
-				{
-					g_nEffectType = atoi(&argv[i][2]);
-					if(g_nEffectType < 0)			g_nEffectType = 255;
-					else if(g_nEffectType > 255)	g_nEffectType = 255;
-					
-					if(bFlag == TRUE)
-					{
-						g_nEffect = Mbset(g_nEffect, 0x7F, Bit_0);
-						puts("拡張グラフィックモード");
-						switch(g_nEffectType)
-						{
-						case 0:
-								puts("Mode=0 as 256x256 color 65536");
-							break;
-						case 1:
-								puts("Mode=1 as 256x256 color 256");
-							break;
-						case 2:
-								puts("Mode=2 as 384x256 color 256");
-							break;
-						case 3:
-								puts("Mode=3 as 512x512 color 16");
-							break;
-						case 4:
-								puts("Mode=4 as 512x512 color 8");
-							break;
-						case 5:
-								puts("Mode=5 as 768x512 color 1");
-							break;
-						default:
-							break;
-						}
-					}
-					continue;
 				}
 				
 				HelpMessage();	/* ヘルプ */
@@ -841,5 +500,3 @@ int16_t main(int16_t argc, int8_t** argv)
 	
 	return ret;
 }
-
-
